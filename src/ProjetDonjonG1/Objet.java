a654c62bd74bdcc6887bc4bb563e8725100c9822
Nauguier <Bnauguier@gmail.com>
2021-03-10 12:28:42 +0100

    Changement de message d'erreur, modifications du setter, ajout de commentaire

--- src/ProjetDonjonG1/Objet.java
+++ src/ProjetDonjonG1/Objet.java
@@ -1,30 +1,29 @@
 package ProjetDonjonG1;
 
-
+// Class abstraite car on ne vas pas instancier un objet c'est abstrait.
 public abstract class Objet  {
 	
+	// variable name pour le nom de l'objet
 	private String name;
 
 	
-	
+	// constructeur de name
 	public Objet(String name) throws ExceptionObjet {
 		setName(name);
 		
 	}
 
 
-	
-	
-	
-
+	//getter de la variable name
 	public String getName() {
 		return name;
 	}
 
 
-
+	// setter de la variable name on test la longueur et si il est composé de au 
+	// moins 1 lettre.
 	public void setName(String name) throws ExceptionObjet {
-		if (name.length() >= 3 && isACaractere(name))
+		if (name.length() >= 2 && (isACaractere(name)== false))
 			this.name = name;
 		else
 			throw new ExceptionObjet(" le nom de l'objet n'est pas bon");
@@ -32,7 +31,8 @@ public abstract class Objet  {
 	
 	
 	
-	
+	// methode qui permet de savoir si la chaine de charactère passé en paramètre
+	// est composé que de chiffre = true, si il y a des lettres = false.
 	public boolean isACaractere(String s) {
 		for (int i= 0; i<s.length(); i++)
 			if(!Character.isDigit(s.charAt(i)))
@@ -41,8 +41,13 @@ public abstract class Objet  {
 			
 	}
 
+	// methode qui permet d'afficher le nom de l'objet.
+	public String toString() {
+		return " le nom de l'objet est "+getName();	
+		}
 	
 	
+	// methode qui va nous permettre d'enlever des pv au personnage.
 	public abstract int modificationPv();
 	
 	
