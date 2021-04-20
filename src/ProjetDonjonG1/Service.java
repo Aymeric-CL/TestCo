85a1b50ef45e7ad5d57d4554cd2ba900de1a3d9b
Alyson <alysonvion@gmail.com>
2021-04-20 19:10:52 +0200

    modification

--- src/ProjetDonjonG1/Service.java
+++ src/ProjetDonjonG1/Service.java
@@ -14,7 +14,7 @@ class Service implements Runnable{
 	private int numClient=0; // contiendra le numéro de client géré par ce thread
 
 	
-	Service(Socket s, Serveur serveur) throws Exception  {// le param s est donnée dans BlablaServ par ss.accept()
+	Service(Socket s, Serveur serveur) throws Exception  {
 		serveur=serveur; // passage de local en global (pour gestion dans les autres méthodes)
 	    s=s; // passage de local en global
 	    
@@ -40,7 +40,7 @@ class Service implements Runnable{
 	    	
 	    	char charCur[] = new char[1]; // declaration d'un tableau de char d'1 element, _in.read() y stockera le char lu
 	    	while(in.read(charCur, 0, 1)!=-1) { // attente en boucle des messages provenant du client (bloquant sur _in.read())
-	    		// on regarde si on arrive a� la fin d'une chaine ...
+	    		// on regarde si on arrive a� la fin d'une chaine ...
 	    		if (charCur[0] != '\u0000' && charCur[0] != '\n' && charCur[0] != '\r') {
 	    			message += charCur[0]; // ... si non, on concatène le caractère dans le message
 	    			}
@@ -51,7 +51,7 @@ class Service implements Runnable{
 	    }
 	    finally  {// finally se produira le plus souvent lors de la deconnexion du client 
 	    	try{
-	    		// on indique a� la console la deconnexion du client
+	    		// on indique a� la console la deconnexion du client
 	    		System.out.println("Le client no "+numClient+" s'est deconnecte");
 	    		s.close(); // fermeture du socket s'il ne l'a pas deja ete (a cause de l'exception levee plus haut)
 	    	}
